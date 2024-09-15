87061343ea7e38a2352b13d2acc3fb2466a29e16
Md Golamur RAhman <arifahammed300@gmail.com>
2024-09-15 18:19:53 +0600

    first commit

---
+++ first.py
@@ -0,0 +1,42 @@
+#print
+print("My name is Arif \n and this is my first project on Python") 
+#Python Indentation
+if 5>7:
+    print("i'ts false")
+
+if 7>3:
+    print("this is true")    
+
+#variable
+x = 5
+y = 22
+z = x+y
+print(z)
+
+#type casting
+x=5             
+y="arif"
+z=5.2
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Booleans
+
+a=20
+b=50
+if a<b:
+    print("b ar man boro")
+else:
+    print("b ar man sotu") 
+
+#operators
+
+a=20
+b=30
+c=40
+print(a<=b)
+print(c>b)
+print(a+b)
+print(a<b and a<c)
+print(a>b or b<c)
